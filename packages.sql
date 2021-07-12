8f97c5ab12c0d21565885565655dad6874b5061b
williamjacksn <williamjacksn@users.noreply.github.com>
2021-07-12 12:56:32 +0000

    Add a file for cffi to the index

--- packages.sql
+++ packages.sql
@@ -274,4 +274,5 @@ INSERT INTO "packages" VALUES('SQLAlchemy','sha256','0ba02b6a4e64b6f371c3692762b
 INSERT INTO "packages" VALUES('Pillow','sha256','6344661a836f0bcc802768c4d3b733605a6ed353cf433abe3e4480872ebc4ec1','https://github.com/alpine-wheels/pillow/releases/download/8.3.0/Pillow-8.3.0-cp39-cp39-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('regex','sha256','7b16235dad9692329550c31e99eaf72624dd9254122fcac44a36fc81d2147b04','https://github.com/alpine-wheels/regex/releases/download/2021.7.1/regex-2021.7.1-cp39-cp39-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('cmake','sha256','fb7bb992a0836fd1141e23704efecbaeed2a9b76fd6c3a9f9a872931f81d7a98','https://github.com/alpine-wheels/cmake/releases/download/3.20.5/cmake-3.20.5-cp39-cp39-linux_x86_64.whl');
+INSERT INTO "packages" VALUES('cffi','sha256','c0f4f2d49e5aa10af9e7f29d1fc129532ad0c49f649b9167570c83597c319695','https://github.com/alpine-wheels/cffi/releases/download/1.14.6/cffi-1.14.6-cp39-cp39-linux_x86_64.whl');
 COMMIT;

4b149ea0046e8db013854eaa778665f0cb053c5e
williamjacksn <williamjacksn@users.noreply.github.com>
2021-07-15 14:28:18 +0000

    Add a file for SQLAlchemy to the index

--- packages.sql
+++ packages.sql
@@ -286,4 +286,5 @@ INSERT INTO "packages" VALUES('opencv-python','sha256','7ad38a628d85690ca2cb5091
 INSERT INTO "packages" VALUES('neo4j','sha256','5643280e91be92529249afb03056140924dc9eac7303002bdc27e5f684cf5762','https://github.com/alpine-wheels/neo4j/releases/download/4.3.3/neo4j-4.3.3-py3-none-any.whl');
 INSERT INTO "packages" VALUES('uvloop','sha256','7108db35509e0dc6f642e26633bc0a657d69a021dd0f8150a57490fe76de59ed','https://github.com/alpine-wheels/uvloop/releases/download/0.15.3/uvloop-0.15.3-cp39-cp39-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('elastic-apm','sha256','b7d58350b37a65706f97ee4525029f8a60dfa22c478c90106872d7a71bd99b91','https://github.com/alpine-wheels/elastic-apm/releases/download/6.3.3/elastic_apm-6.3.3-py3-none-any.whl');
+INSERT INTO "packages" VALUES('SQLAlchemy','sha256','cdc41df2650c0556eaecf567864967fe690e0f262c30bee6906478e8af7190f9','https://github.com/alpine-wheels/sqlalchemy/releases/download/1.4.21/SQLAlchemy-1.4.21-cp39-cp39-linux_x86_64.whl');
 COMMIT;

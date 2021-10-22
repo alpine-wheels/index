60edacaf9a96ef9068fe3df700ccce3934a073fc
williamjacksn <williamjacksn@users.noreply.github.com>
2021-10-22 13:56:33 +0000

    Add a file for SQLAlchemy to the index

--- packages.sql
+++ packages.sql
@@ -348,4 +348,5 @@ INSERT INTO "packages" VALUES('greenlet','sha256','8bd7d683e1b3b1bbe908fc7273fc5
 INSERT INTO "packages" VALUES('elastic-apm','sha256','d899a63aae0c3ac29f34e0bd6e03d14777256fb9aab06f0f01de3f7ddcf879d2','https://github.com/alpine-wheels/elastic-apm/releases/download/6.6.0/elastic_apm-6.6.0-cp310-cp310-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('wrapt','sha256','6026a6bf79bb6ef1930d275ecc881fdb7535ba6c3932370e2b016407989b833e','https://github.com/alpine-wheels/wrapt/releases/download/1.13.2/wrapt-1.13.2-cp310-cp310-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('regex','sha256','507d50a53721ee9ce13bcb963cf47e6d0a22216b825448e39631a2616bfdbc9b','https://github.com/alpine-wheels/regex/releases/download/2021.10.21/regex-2021.10.21-cp310-cp310-linux_x86_64.whl');
+INSERT INTO "packages" VALUES('SQLAlchemy','sha256','11408f105ea8a56233120ebbfb979093dde0536f2dabd5e98c0c864d10c4d00f','https://github.com/alpine-wheels/sqlalchemy/releases/download/1.4.26/SQLAlchemy-1.4.26-cp310-cp310-linux_x86_64.whl');
 COMMIT;

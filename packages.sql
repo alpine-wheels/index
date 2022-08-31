e9972851a3af8d530c003b92ee923a47f3dc99d9
williamjacksn <williamjacksn@users.noreply.github.com>
2022-08-31 18:55:58 +0000

    Add a file for neo4j to the index

--- packages.sql
+++ packages.sql
@@ -473,4 +473,5 @@ INSERT INTO "packages" VALUES('SQLAlchemy','sha256','527a56dfb92efd764c365ef438b
 INSERT INTO "packages" VALUES('borgbackup','sha256','3b82a27c0c9796b0240cf3148ec38cb0c222febcb541253ae3f766a875c85948','https://github.com/alpine-wheels/borgbackup/releases/download/1.2.2/borgbackup-1.2.2-cp310-cp310-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('greenlet','sha256','ee604dc160af4df42d99d4198dd9f9d2320280e9196df985ecfa17507340780a','https://github.com/alpine-wheels/greenlet/releases/download/1.1.3/greenlet-1.1.3-cp310-cp310-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('scipy','sha256','b9e0f1df491f8becc186597a9046a5fdcde8688a214300680050e7ce393bc9eb','https://github.com/alpine-wheels/scipy/releases/download/1.9.1/scipy-1.9.1-cp310-cp310-linux_x86_64.whl');
+INSERT INTO "packages" VALUES('neo4j','sha256','bf7aae507df0f09795e43aeae296ede3bcb5d7688e70548357d29908a6ce1e34','https://github.com/alpine-wheels/neo4j/releases/download/4.4.6/neo4j-4.4.6-py3-none-any.whl');
 COMMIT;

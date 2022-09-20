157442e2193db78747ec5a3fb90f05efb3f5512f
williamjacksn <williamjacksn@users.noreply.github.com>
2022-09-20 20:33:29 +0000

    Add a file for neo4j to the index

--- packages.sql
+++ packages.sql
@@ -483,4 +483,5 @@ INSERT INTO "packages" VALUES('numpy','sha256','138ac8312856cefd9ea07ba59ec3af15
 INSERT INTO "packages" VALUES('numpy','sha256','60e72967f4c18a3f52bfe6c084b128f33891399345dff6974aa6ef8046d4de54','https://github.com/alpine-wheels/numpy/releases/download/1.23.3/numpy-1.23.3-cp39-cp39-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('pandas','sha256','d5e1ebcf5d566e852f82bd21dc1d3d6dd0533546023faa700604eed4efd0611d','https://github.com/alpine-wheels/pandas/releases/download/1.5.0/pandas-1.5.0-cp39-cp39-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('pandas','sha256','92cbeb14ed04aff0d13bfa89b13b90e53d0a36459ee6c1cdce6df0531f8d9934','https://github.com/alpine-wheels/pandas/releases/download/1.5.0/pandas-1.5.0-cp310-cp310-linux_x86_64.whl');
+INSERT INTO "packages" VALUES('neo4j','sha256','bef01c8b1d3cb35989a4e6225beba27f55f1c45190986c338425831dd5345687','https://github.com/alpine-wheels/neo4j/releases/download/5.0.1/neo4j-5.0.1-py3-none-any.whl');
 COMMIT;

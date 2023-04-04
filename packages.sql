9bb455f4203687ceaee7cff0cf7fe2555810ebfb
williamjacksn <williamjacksn@users.noreply.github.com>
2023-04-04 14:32:36 +0000

    Add a file for neo4j to the index

--- packages.sql
+++ packages.sql
@@ -544,4 +544,5 @@ INSERT INTO "packages" VALUES('scikit-image','sha256','16f79fc90e2fed46cc9b4bb1b
 INSERT INTO "packages" VALUES('scikit-learn','sha256','2e784e135184639bc1ee62d526684e9d4b5175eab2dc0df513c1bb0bc6a19c5b','https://github.com/alpine-wheels/scikit-learn/releases/download/1.2.2/scikit_learn-1.2.2-cp311-cp311-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('psycopg2','sha256','368bfab3c3d6be85a74dfdb5ecec0b6d3acdd82187ba0de9f976659fa0151149','https://github.com/alpine-wheels/psycopg2/releases/download/2.9.5/psycopg2-2.9.5-cp310-cp310-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('borgbackup','sha256','2b378fb4ce934632649b7e11e6400a4aa0b271766e00980e27f6f6c207575d6f','https://github.com/alpine-wheels/borgbackup/releases/download/1.2.4/borgbackup-1.2.4-cp311-cp311-linux_x86_64.whl');
+INSERT INTO "packages" VALUES('neo4j','sha256','7f43c020ae0fe4985bd57d9f25e175d1960e9502bd41433024877293c4ce7737','https://github.com/alpine-wheels/neo4j/releases/download/5.7.0/neo4j-5.7.0-py3-none-any.whl');
 COMMIT;

caf8f0e7951698a38f18573e6b55672fd7f3c9df
williamjacksn <williamjacksn@users.noreply.github.com>
2023-06-20 18:45:28 +0000

    Add a file for numpy to the index

--- packages.sql
+++ packages.sql
@@ -570,4 +570,5 @@ INSERT INTO "packages" VALUES('pandas','sha256','372b01cfd1e058467d6d9ff5c5d37ce
 INSERT INTO "packages" VALUES('pandas','sha256','dafeac13842223341c8e8b43acef64bb8248291405aed4921a6c6ef9d9325006','https://github.com/alpine-wheels/pandas/releases/download/2.0.2/pandas-2.0.2-cp310-cp310-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('pandas','sha256','24a93f2a9db9937bf955ff32f7267a629eb466f4e921ce7b58092c5306e68c47','https://github.com/alpine-wheels/pandas/releases/download/2.0.2/pandas-2.0.2-cp311-cp311-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('scikit-image','sha256','60a7ee2eb04fb4e3492606c91f88af3669d905e3659d8514abedb7695fdf2b5a','https://github.com/alpine-wheels/scikit-image/releases/download/0.21.0/scikit_image-0.21.0-cp311-cp311-linux_x86_64.whl');
+INSERT INTO "packages" VALUES('numpy','sha256','bd82dd8fd4e9767ac3c76a38eef9cb82e201f3b5d2910837759d95a303c97574','https://github.com/alpine-wheels/numpy/releases/download/1.23.2/numpy-1.23.2-cp39-cp39-linux_x86_64.whl');
 COMMIT;

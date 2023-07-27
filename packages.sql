407a03ec8065e2fbe6a4554e04b3fd72d6fc407c
williamjacksn <williamjacksn@users.noreply.github.com>
2023-07-27 19:08:48 +0000

    Add a file for psycopg2 to the index

--- packages.sql
+++ packages.sql
@@ -582,4 +582,5 @@ INSERT INTO "packages" VALUES('neo4j','sha256','389ee582139e30fd13f180ea6b380b26
 INSERT INTO "packages" VALUES('scikit-learn','sha256','3fe7f633c4b1d6f9b97fc71f1b81c91d8d0c758e3cc0651a310a15555e90fbeb','https://github.com/alpine-wheels/scikit-learn/releases/download/1.3.0/scikit_learn-1.3.0-cp311-cp311-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('opencv-python','sha256','75c1dfd1bf83988c83fa4a293915bc47a56fbbc70d6e151b7fedfd0b9fc9b1b5','https://github.com/alpine-wheels/opencv-python/releases/download/4.8.0.74/opencv_python-4.8.0.74-cp311-cp311-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('PyYAML','sha256','1fffbf51a6c1e9997614d10ffbea16365d7f7a2fe710de39fd2ce26930f7022d','https://github.com/alpine-wheels/pyyaml/releases/download/6.0.1/PyYAML-6.0.1-cp311-cp311-linux_x86_64.whl');
+INSERT INTO "packages" VALUES('psycopg2','sha256','f1825efef7936b5fca33d80b1f1b8066048dc0409aaca20925069ade5ca0c03c','https://github.com/alpine-wheels/psycopg2/releases/download/2.9.6/psycopg2-2.9.6-cp311-cp311-linux_aarch64.whl');
 COMMIT;

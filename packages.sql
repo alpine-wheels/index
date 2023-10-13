e085e35a8398e6228dab4b499b9321a4bfcdcdae
williamjacksn <williamjacksn@users.noreply.github.com>
2023-10-13 20:20:35 +0000

    Add a file for zodbpickle to the index

--- packages.sql
+++ packages.sql
@@ -613,4 +613,5 @@ INSERT INTO "packages" VALUES('psycopg2','sha256','9003991e77b9817e380f84bc4c86b
 INSERT INTO "packages" VALUES('psycopg2','sha256','cae335bf7d48ff941154de965d00a5f55bb140cba9f79e8e26eb0defe5f75c89','https://github.com/alpine-wheels/psycopg2/releases/download/2.9.9/psycopg2-2.9.9-cp311-cp311-linux_aarch64.whl');
 INSERT INTO "packages" VALUES('cffi','sha256','5ac3743901fbea3b31f122086cceee765fe55d3fc10c92528c356d28612df552','https://github.com/alpine-wheels/cffi/releases/download/1.16.0/cffi-1.16.0-cp312-cp312-linux_aarch64.whl');
 INSERT INTO "packages" VALUES('zope.interface','sha256','a8483be35f526dde256e043897547fa2e29e46409a56952570ab564775a2950c','https://github.com/alpine-wheels/zope.interface/releases/download/6.1/zope.interface-6.1-cp312-cp312-linux_x86_64.whl');
+INSERT INTO "packages" VALUES('zodbpickle','sha256','38fca654cc8dee447e31bb631a005749278158b3c60b1b67d42ddff8f1f69bb8','https://github.com/alpine-wheels/zodbpickle/releases/download/3.1/zodbpickle-3.1-cp312-cp312-linux_x86_64.whl');
 COMMIT;

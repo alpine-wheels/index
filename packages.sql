f16ace703dc184487bea3bf4316a2b9b2fbd3c52
williamjacksn <565174+williamjacksn@users.noreply.github.com>
2024-04-17 15:46:18 +0000

    Add a file for zodbpickle to the index

--- packages.sql
+++ packages.sql
@@ -669,4 +669,5 @@ INSERT INTO "packages" VALUES('pymupdf','sha256','9afae04b1a635866c569621c2832ed
 INSERT INTO "packages" VALUES('scikit-image','sha256','3e414819e019341c7727c777b713470656dbb4dbe5f48c2f237a9a685473d284','https://github.com/alpine-wheels/scikit-image/releases/download/0.23.1/scikit_image-0.23.1-cp312-cp312-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('zope.interface','sha256','548f5ebbadb8278b61eaaa71c709a61f2abddbe92fe3b2f99fd27ad07a9a0ca0','https://github.com/alpine-wheels/zope.interface/releases/download/6.3/zope.interface-6.3-cp312-cp312-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('scikit-learn','sha256','26c0ea26e54f79c5f3b3c8f341d1e08d2edfcbdec2cb91581b564847b979b6b9','https://github.com/alpine-wheels/scikit-learn/releases/download/1.4.2/scikit_learn-1.4.2-cp312-cp312-linux_x86_64.whl');
+INSERT INTO "packages" VALUES('zodbpickle','sha256','65b1fafa3e9331f281de379ce137b7619af9c07debc53b5a23b8763ee32d3fcb','https://github.com/alpine-wheels/zodbpickle/releases/download/3.3/zodbpickle-3.3-cp312-cp312-linux_x86_64.whl');
 COMMIT;

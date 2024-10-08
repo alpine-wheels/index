cb2b86b4538168455529877109d252e939027f81
williamjacksn <565174+williamjacksn@users.noreply.github.com>
2024-10-08 12:14:27 +0000

    Add a file for zodbpickle to the index

--- packages.sql
+++ packages.sql
@@ -749,4 +749,5 @@ INSERT INTO "packages" VALUES('scikit-learn','sha256','6a3edd6c61a960dd2c5249aa4
 INSERT INTO "packages" VALUES('persistent','sha256','c2801570def8b364702d2ee0f5d10e80ebe84ec411427720cb88e18bfbad2097','https://github.com/alpine-wheels/persistent/releases/download/6.1/persistent-6.1-cp312-cp312-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('zodbpickle','sha256','655190bf174f407cdaaef21fd1704f213ecbdecf97e1b18fb22352826b9cbb38','https://github.com/alpine-wheels/zodbpickle/releases/download/4.1/zodbpickle-4.1-cp312-cp312-linux_x86_64.whl');
 INSERT INTO "packages" VALUES('btrees','sha256','e0a3e71ab43ca6c5635610c697eecb204fb6c2e00bd8c5532c3e3389ad89664e','https://github.com/alpine-wheels/btrees/releases/download/6.1/BTrees-6.1-cp312-cp312-linux_x86_64.whl');
+INSERT INTO "packages" VALUES('zodbpickle','sha256','b04dffca1029c29b91b90ad4c38f97a32561c642ae3bc49c930e5f213e22960c','https://github.com/alpine-wheels/zodbpickle/releases/download/4.1.1/zodbpickle-4.1.1-cp312-cp312-linux_x86_64.whl');
 COMMIT;
